e3328f1d8155767bab314b7eb56f5a9ba918abf0
Kimkhue <khuenguyennk@gmail.com>
2026-03-06 04:17:22 +0700

    4:17 AM

--- main.js
+++ main.js
@@ -10,16 +10,70 @@ document.getElementById('pay1').onclick = function () {
         localStorage.setItem("donHang", JSON.stringify(donHang));
     }
 }
+document.getElementById('save1').onclick = function () {
+    let gio = JSON.parse(localStorage.getItem("gioHang")) || [];
+
+    gio.push({
+        ten: "Áo khoác nỉ dành cho nữ",
+        gia: 100000,
+        soLuong: 1
+    });
+
+    localStorage.setItem("gioHang", JSON.stringify(gio));
+
+    alert("Đã thêm vào giỏ hàng!");
+}
+
 
 document.getElementById('pay2').onclick = function () {
     let kq = confirm("Bạn có chắc là muốn mua ngay lập tức không?");
     if (kq == true) {
         alert("Mua hàng thành công!");
         let donHang = {
-            ten: "Áo khoác nỉ dành cho nữ",
+            ten: "Áo khoác nỉ dài tay",
             gia: 100000,
             trangThai: "Đang vận chuyển"
         };
         localStorage.setItem("donHang", JSON.stringify(donHang));
     }
+}
+document.getElementById('save2').onclick = function () {
+    let gio = JSON.parse(localStorage.getItem("gioHang")) || [];
+
+    gio.push({
+        ten: "Áo khoác nỉ dài tay",
+        gia: 100000,
+        soLuong: 1
+    });
+
+    localStorage.setItem("gioHang", JSON.stringify(gio));
+
+    alert("Đã thêm vào giỏ hàng!");
+}
+
+
+document.getElementById('pay3').onclick = function () {
+    let kq = confirm("Bạn có chắc là muốn mua ngay lập tức không?");
+    if (kq == true) {
+        alert("Mua hàng thành công!");
+        let donHang = {
+            ten: "Áo khoác nữ",
+            gia: 100000,
+            trangThai: "Đang vận chuyển"
+        };
+        localStorage.setItem("donHang", JSON.stringify(donHang));
+    }
+}
+document.getElementById('save3').onclick = function () {
+    let gio = JSON.parse(localStorage.getItem("gioHang")) || [];
+
+    gio.push({
+        ten: "Áo khoác nữ",
+        gia: 100000,
+        soLuong: 1
+    });
+
+    localStorage.setItem("gioHang", JSON.stringify(gio));
+
+    alert("Đã thêm vào giỏ hàng!");
 }
