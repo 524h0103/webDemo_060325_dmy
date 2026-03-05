c2bbb8bf0a7b59982c99a0cc99615d3abfb20c5c
Kimkhue <khuenguyennk@gmail.com>
2026-03-06 03:53:07 +0700

    3:53 AM

--- main.js
+++ main.js
@@ -1,3 +1,25 @@
-document.getElementById('btn').onclick = function () {
-    alert('clicked (1)')
+document.getElementById('pay1').onclick = function () {
+    let kq = confirm("Bạn có chắc là muốn mua ngay lập tức không?");
+    if (kq == true) {
+        alert("Mua hàng thành công!");
+        let donHang = {
+            ten: "Áo khoác nỉ dành cho nữ",
+            gia: 100000,
+            trangThai: "Đang vận chuyển"
+        };
+        localStorage.setItem("donHang", JSON.stringify(donHang));
+    }
+}
+
+document.getElementById('pay2').onclick = function () {
+    let kq = confirm("Bạn có chắc là muốn mua ngay lập tức không?");
+    if (kq == true) {
+        alert("Mua hàng thành công!");
+        let donHang = {
+            ten: "Áo khoác nỉ dành cho nữ",
+            gia: 100000,
+            trangThai: "Đang vận chuyển"
+        };
+        localStorage.setItem("donHang", JSON.stringify(donHang));
+    }
 }
